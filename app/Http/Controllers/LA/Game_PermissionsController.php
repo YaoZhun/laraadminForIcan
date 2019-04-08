99b1ac0fd65a3fb8159e36d4f5987dfa80e4515d
Yaozhun <moapckp47557@gmail.com>
2019-04-08 14:55:15 +0800

    控制器引進設定

---
+++ app/Http/Controllers/LA/Game_PermissionsController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http\Controllers\LA;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+use DB;
+use Validator;
+use Datatables;
+use Collective\Html\FormFacade as Form;
+use Dwij\Laraadmin\Models\Module;
+use Dwij\Laraadmin\Models\ModuleFields;
+
+use App\Models\User;
+
+class Game_PermissionsController extends Controller
+{
+    
+}
